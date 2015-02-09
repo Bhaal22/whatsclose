49b942d3218885e3ea66354f69de893f5567fd09
Bhaal22 <muller.john@gmail.com>
2015-02-09 19:41:21 +0100

    review code layout

--- src/www/app.js
+++ src/www/app.js
@@ -17,7 +17,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true 
+    extended: true 
 }));
 
 app.use(bodyParser.json() );
@@ -28,26 +28,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 console.log("setting helpers");
 helpers(app);
 
-
-/*
- * Index
- */
 app.get('/', function(req, res) {
-	var params = {};
-	params.title = "Whatsclose.io";
-	
-	searcher.getAllStyles()
-		.then(function(data) {
-			
-      params.styles = data;
-
-			res.render('index', params);
-			
-		}, function(error) {
-			console.log("error: %s", error);
-			
-			res.render('index', params);
-		});
+    var params = {};
+    params.title = "Whatsclose.io";
+    
+    searcher.getAllStyles()
+	.then(function(data) {
+	    params.styles = data;	    
+	    res.render('index', params);
+	    
+	}, function(error) {
+	    console.log("error: %s", error);
+	    
+	    res.render('index', params);
+	});
 
 });
 
@@ -60,40 +54,40 @@ app.use('/api', router);
  * Ajax call to search band name
  */
 app.get('/bandSearch', function(req, res) {	
-	searcher.search(req.query)
-		.then(function(data) {
-			res.send(data);
-		}, function(err) {
-			console.log(err);
-			res.status(500).send(err);
-		});
-	
+    searcher.search(req.query)
+	.then(function(data) {
+	    res.send(data);
+	}, function(err) {
+	    console.log(err);
+	    res.status(500).send(err);
+	});
+    
 });
 
 /*
  * Get ip to find geoloc ? or use geoloc api from HTML 5 ?
  */
 function getClientIp(req) {
-	var ipAddress;
+    var ipAddress;
 
-	var forwardedIpsStr = req.header('x-forwarded-for');
-	if (forwardedIpsStr) {
-		var forwardedIps = forwardedIpsStr.split(',');
-		ipAddress = forwardedIps[0];
-	}
-	if (!ipAddress) {
-		ipAddress = req.connection.remoteAddress;
-	}
-	
-	return ipAddress;
+    var forwardedIpsStr = req.header('x-forwarded-for');
+    if (forwardedIpsStr) {
+	var forwardedIps = forwardedIpsStr.split(',');
+	ipAddress = forwardedIps[0];
+    }
+    if (!ipAddress) {
+	ipAddress = req.connection.remoteAddress;
+    }
+    
+    return ipAddress;
 };
 
 module.exports.startServer = function(port, hostname) {
-  if (hostname === undefined)
-    hostname = '127.0.0.1';
+    if (hostname === undefined)
+	hostname = '127.0.0.1';
 
-	server.listen(port, hostname, function() {
-		console.log('listening on *: %s %s', hostname, port);
-	});
+    server.listen(port, hostname, function() {
+	console.log('listening on *: %s %s', hostname, port);
+    });
 };
 
