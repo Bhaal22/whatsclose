b3e3b146dd1b34a2fa81ee57062275ed62eb8e8d
Bhaal22 <muller.john@gmail.com>
2015-04-14 23:51:31 +0200

    linked Search function event

--- src/www/public/components/search/views/SimpleSearchView.js
+++ src/www/public/components/search/views/SimpleSearchView.js
@@ -26,7 +26,7 @@ define([
       this.el = options.location;
       this.htmlID = _.uniqueId();
 
-      _.bindAll(this, 'render');
+      _.bindAll(this, 'render', '_reset', '_search', '_updateLocation', '_validate');
 
       
       
@@ -43,7 +43,91 @@ define([
 
       var datePickerView = new DatePickerView({ 'vent': this.vent, 'location': '#datePicker-fields' });
       datePickerView.render ();
-		}
+		},
+
+    _validate: function(elt) {
+      //iterate through components to get all models to give to validate their content
+      
+      
+    },
+
+    _search: function() {
+      //iterate through components to get all models to give to search REST API
+
+      var self = this;
+
+      self._validate(elt);
+         
+      var concerts = new Concerts ();
+
+      this._reset(elt).done(function() {
+     
+        self._updateLocation(elt).done(function(_location) {
+          var location = _location.lat() + "," + _location.lng();
+          
+          concerts.fetch ({ 
+            data: { 
+              bandNames: $(self.band).val(),
+              location: location,
+              from: $(self.from).val(),
+              to: $(self.to).val(),
+              radius: $(self.radius).val()
+            },
+            success: function (collection, response) {
+              console.dir(response);
+              console.dir(collection);
+              
+              self.vent.trigger('concertsRetrieved', {
+                query: {
+                  from: $(self.from).val()
+                },
+                concerts: response
+              });
+            }
+          })
+        })
+      });
+
+      
+    },
+
+    _reset: function(elt) {
+
+      console.log('SearchView resetting ....');
+      var self = this;
+      var deferred = $.Deferred();
+      
+      self.vent.trigger('resetMap');
+      
+      deferred.resolve();
+      return deferred;
+    },
+
+    _updateLocation : function(elt) {
+
+      var deferred = $.Deferred();
+
+      var self = this;
+      //var address = $(this.location).val();
+      //var radius = $(this.radius).val();
+
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({'address': address}, function (results, status){
+        if (status === google.maps.GeocoderStatus.OK){
+          self.vent.trigger('updateLocation', {
+            location: results[0].geometry.location,
+            radius: radius
+          });
+
+          console.log(results[0].geometry.location);
+          deferred.resolve (results[0].geometry.location);
+        }
+        else
+          deferred.reject();
+      });
+
+      return deferred.promise();
+    }
 
 	});
   return view;
