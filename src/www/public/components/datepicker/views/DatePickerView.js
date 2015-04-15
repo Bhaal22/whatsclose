33d3e4567f9c5158429d71afe8530dbff7a708af
Bhaal22 <muller.john@gmail.com>
2015-04-15 22:59:55 +0200

    updated DatePicker Component

--- src/www/public/components/datepicker/views/DatePickerView.js
+++ src/www/public/components/datepicker/views/DatePickerView.js
@@ -5,7 +5,9 @@ define([
   'underscore',
   'backbone',
   'text!/components/datepicker/templates/template.html',
-], function($, _, Backbone) {
+  '/components/datepicker/js/bootstrap-datepicker.min.js',
+  'css!/components/datepicker/css/datepicker3.css'
+], function($, _, Backbone, template) {
   var view = Backbone.View.extend({
 
     initialize: function(options) {
@@ -14,6 +16,23 @@ define([
       this.el = options.location;
       
       _.bindAll(this, 'render');
+    },
+
+    render: function() {
+      $(this.el).html(template);
+
+      var today = new Date();
+      $('.input-daterange').datepicker({
+        format: "yyyy-mm-dd",
+        todayHighlight: true,
+        multidate: false,
+        toggleActive: true
+      });
+
+      $('#from-input').datepicker('update', today);
+
+      today.setDate(today.getDate() + 21);
+      $('#to-input').datepicker('update', today);
     }
     
   });
