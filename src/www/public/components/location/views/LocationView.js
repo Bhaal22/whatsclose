bd499d476559d5cb32b7cdbbfeeebcd6b0555a57
Bhaal22 <muller.john@gmail.com>
2015-07-07 23:20:00 +0200

    use input group to have Location + Distance in the same component

--- src/www/public/components/location/views/LocationView.js
+++ src/www/public/components/location/views/LocationView.js
@@ -41,7 +41,7 @@ define([
 
       $(".dropdown-menu li a").click(function(){
         var selText = $(this).text();
-        $(this).parents('.dropdown').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+        $(this).parents('.input-group-btn').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
       });
 
 $("#btnSearch").click(function(){
