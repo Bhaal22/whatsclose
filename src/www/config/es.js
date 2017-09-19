07e9b442553d1c04fc30c3696544a276dbe64022
Bhaal22 <muller.john@gmail.com>
2017-09-19 02:33:04 +0200

    fixed app-config

---
+++ src/www/config/es.js
@@ -0,0 +1,6 @@
+module.exports = {
+    hostname : "localhost",
+    port : '9200',
+    index : 'whatsclose',
+    type: 'concert'
+};
---
+++ src/www/config/settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+    web : {
+      port: 3000,
+      isHttp: true
+    },
+    version : {
+        major: 0,
+        minor: 0,
+        build: 2
+    },
+    application:{
+        name: "whatsclose api",
+        code_name: "Roth"
+    }
+};
